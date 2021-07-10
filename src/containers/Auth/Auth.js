842f4fca93257c69e995e775080b5b6e9907ccd4
Oleg Kireev <flucky@mail.ru>
2021-07-10 11:00:19 +0300

    Первичная разметка страницы входа (компонент Auth).

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react';
+import classes from './Auth.module.scss';
+import Button from '../../components/UI/Button/Button';
 
 export default class Auth extends Component {
+  loginHandle = () => {};
+
+  registerHandle = () => {};
+
+  submitHandle = (e) => {
+    e.preventDefault();
+  };
+
   render() {
     return (
-      <div>
-        <h1>Auth</h1>
+      <div className={classes.Auth}>
+        <div>
+          <h1>Авторизация</h1>
+          <form className={classes.AuthForm} onSubmit={this.submitHandle}>
+            <input type="text" />
+            <input type="text" />
+
+            <Button type="success" onClick={this.loginHandle}>
+              Войти
+            </Button>
+            <Button type="primary" onClick={this.registerHandle}>
+              Зарегистрироваться
+            </Button>
+          </form>
+        </div>
       </div>
     );
   }
