45065d75d08b299426c586ed58ba43fdc77e79b1
Oleg Kireev <flucky@mail.ru>
2021-07-14 15:22:57 +0300

    Компонент QuizList выводит квизы из БД

--- src/containers/QuizList/QuizList.js
+++ src/containers/QuizList/QuizList.js
@@ -1,16 +1,43 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import { NavLink } from 'react-router-dom';
 import classes from './QuizList.module.scss';
 
 export default class QuizList extends Component {
+  state = {
+    quizes: [],
+  };
+
   renderQuizes() {
-    return [1, 2, 3].map((quiz, index) => {
+    return this.state.quizes.map((quiz) => {
       return (
-        <li key={index}>
-          <NavLink to={`/quiz/${quiz}`}>Квиз {quiz}</NavLink>
+        <li key={quiz.id}>
+          <NavLink to={`/quiz/${quiz.id}`}>Квиз №{quiz.name}</NavLink>
         </li>
       );
     });
+  }
+
+  async componentDidMount() {
+    try {
+      const response = await axios.get(
+        'https://react-quiz-e74fd-default-rtdb.europe-west1.firebasedatabase.app/quizes.json'
+      );
+
+      const quizes = [];
+      Object.keys(response.data).forEach((key, index) => {
+        quizes.push({
+          id: key,
+          name: `${index + 1}`,
+        });
+      });
+
+      this.setState({
+        quizes,
+      });
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   render() {
