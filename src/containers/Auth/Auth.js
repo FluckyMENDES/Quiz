c87fa1ebf0cad7600aa00f58ecef424bc0b214a4
Oleg Kireev <flucky@mail.ru>
2021-07-20 11:47:52 +0300

    Компонент Auth переписан на использование Redux.

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -2,9 +2,10 @@ import React, { Component } from 'react';
 import classes from './Auth.module.scss';
 import Button from '../../components/UI/Button/Button';
 import Input from '../../components/UI/Input/Input';
-import axios from 'axios';
+import { connect } from 'react-redux';
+import { auth } from './../../store/actions/auth';
 
-export default class Auth extends Component {
+class Auth extends Component {
   state = {
     isFormValid: false,
     formControls: {
@@ -97,38 +98,20 @@ export default class Auth extends Component {
     this.setState({ formControls, isFormValid });
   };
 
-  loginHandle = async () => {
-    const authData = {
-      email: this.state.formControls.email.value,
-      password: this.state.formControls.password.value,
-      returnSecureToken: true,
-    };
-    try {
-      const response = await axios.post(
-        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDBUPNHSoN73QnV9uuXAsJV_1cc_8yBk6A',
-        authData
-      );
-      console.log(response.data);
-    } catch (error) {
-      console.error(error);
-    }
+  loginHandle = () => {
+    this.props.auth(
+      this.state.formControls.email.value,
+      this.state.formControls.password.value,
+      true
+    );
   };
 
-  registerHandle = async () => {
-    const authData = {
-      email: this.state.formControls.email.value,
-      password: this.state.formControls.password.value,
-      returnSecureToken: true,
-    };
-    try {
-      const response = await axios.post(
-        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDBUPNHSoN73QnV9uuXAsJV_1cc_8yBk6A',
-        authData
-      );
-      console.log(response.data);
-    } catch (error) {
-      console.error(error);
-    }
+  registerHandle = () => {
+    this.props.auth(
+      this.state.formControls.email.value,
+      this.state.formControls.password.value,
+      false
+    );
   };
 
   submitHandle = (e) => {
@@ -154,3 +137,11 @@ export default class Auth extends Component {
     );
   }
 }
+
+function mapDispatchToProps(dispatch) {
+  return {
+    auth: (email, password, isLogin) => dispatch(auth(email, password, isLogin)),
+  };
+}
+
+export default connect(null, mapDispatchToProps)(Auth);
---
+++ src/firebase/url.js
@@ -0,0 +1,4 @@
+export const LOGIN_URL =
+  'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDBUPNHSoN73QnV9uuXAsJV_1cc_8yBk6A';
+export const REGISTER_URL =
+  'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDBUPNHSoN73QnV9uuXAsJV_1cc_8yBk6A';
